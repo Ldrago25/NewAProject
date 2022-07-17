b9c0af55b4e532871029655d7ae46e358dc0768d
21harley <jhonharleyllanesescobar@hotmail.com>
2022-07-16 22:21:05 -0400

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hola mundo :v </h1>
+      <button className='btn btn-primary'>Hola</button>
+    </div>
+  )
+}
+
+export default App
