fc666ed7cb4c2b83ed0a50e3d62654a3d7c29e0d
Endersson Enrique Garcia B <endersson.garcia.eegb@gmail.com>
2022-07-17 22:57:13 -0400

    login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -21,18 +21,36 @@ const Login=()=>{
 
     }
     return(
-     <div>
-         <form onSubmit={(e)=>{onsubmit(e)}}>
-            <label htmlFor="">
-               Email:<input type="email" name="email" />
-            </label>
-            <label htmlFor="">
-               Password:<input type="password" name="password" />
-            </label>
-            <button >Login</button>
-         </form>
-         <Link to={"/register"}>register</Link>
-     </div>
+     <div className="container">
+      <div className="row  vh-100 justify-content-center align-items-center">
+        <div className="col-auto bg-light p-5 shadow-lg">
+          <div className="card-header">
+            <h1 className='text-secondary'>Log in</h1>
+          </div>
+          <form className="border-bottom border-secondary p-4 form " onSubmit={(e) => handelSutmid(e)}>
+            <div className="mb-3">
+              <input
+                className="form-control mb-3"
+                type="email"
+                name="email"
+                placeholder='Email'
+                />
+            </div>
+            <div className="mb-3">
+              <input
+                className="form-control mb-3"
+                type="password"
+                name="password"
+                placeholder='******'
+              />
+            </div>
+            <button className="form-login__load btn btn-primary">iniciar sesion</button>
+          </form>
+           <Link to={"/register"}>register</Link>
+          <p className="small text-center">Do not have an Account?<Link to="/register">Register</Link></p>
+        </div>
+      </div>
+    </div>
     ) 
  }
  
