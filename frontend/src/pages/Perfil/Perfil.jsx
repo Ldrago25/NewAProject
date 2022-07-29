37ccc6f2bce45f1f1d104f9abebefb2e94f7eb53
Endersson Enrique Garcia B <endersson.garcia.eegb@gmail.com>
2022-07-29 01:46:15 -0400

    actualizacion perfil

--- frontend/src/pages/Perfil/Perfil.jsx
+++ frontend/src/pages/Perfil/Perfil.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
-
+import React, { useEffect,useState } from 'react'
+import { getUser } from '../../helpers/getUser';
+import WorkGridItem from './WorkGridItem';
+import perfil from "../../img/perfil.jpg";
 const Perfil = () => {
-  return (
-    
-    <div className='col' >
-        <div className='card' style={{ width: 180, margin:10 }}>
-            <img className='' src={img.url} alt={img.name} />
-            <div className='card-body'>
-                
-                <p className='card-text'>{img.name}</p>
-                
-                {images &&(
-                <button 
-                    className='btn btn-success btn-sm'
-                    onClick={()=>addUser(img)}>
-                    Add to Team
-                </button>
-                ) }
-               
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        getUser().then(setImages);
+    }, [])
+    return (
+        <div className=' bg-light container'>
+            <div className="d-flex justify-content-center align-items-center">
+            <div style={{ width: 180,height:180, margin:10, display: "flex", justifyContent: "center"}}>
+                <img src={perfil} alt="" style={{borderRadius: "50%"}} className='img-responsive'/>
+                <p className='mt-5 mx-2'>UserName</p>
             </div>
+            
+            
+           
+        </div>              
+            <div className="row">
+            {
+                images.map(img=>(
+                    <WorkGridItem 
+                        key={img.id}
+                        img={img}
+                        images={images}/>
+                ))
+            }
+            </div>  
         </div>
-    </div>
-    
-
-)
-}
+      )
 }
 
 export default Perfil
