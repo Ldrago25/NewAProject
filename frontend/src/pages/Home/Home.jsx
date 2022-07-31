4812c95797c15050574867d4bd97021cc4f2feb9
CarlosAl96 <carlos96cacv@gmail.com>
2022-07-31 00:26:53 -0400

    CC: Actualizacion del front

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,30 +3,32 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setLogin } from "./../../reducers/slices/User";
 import Login from "./../Login/Login";
+import Header from "../../components/Header";
+import "./Home.css";
 
-const Home=()=>{
-    const dispatch = useDispatch();
-    const { data,login } = useSelector((state) => state.user);
-    const navigate = useNavigate();
-    const CerrarSeccion=()=>{
-        dispatch(setLogin(false));
-        navigate("/login");
-    }
-    return(
-        <>
-        {
-            (login)?(
-                <div>
-                  Hola Home
-                  <Link to={"/RegisterPost"}>Registrar Post</Link>
-                  <button onClick={()=>{CerrarSeccion()}}>Cerrar Seccion</button>
-                </div>
-            ):(
-             <Login></Login>
-            )
-        }
-        </>
-
-    )
-}
+const Home = () => {
+  const dispatch = useDispatch();
+  const { data, login } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const CerrarSeccion = () => {
+    dispatch(setLogin(false));
+    navigate("/login");
+  };
+  return (
+    <>
+      {login ? (
+        <div>
+          <Header></Header>
+          <div className="cont">
+            <div className="posts">
+              <Link className="btn btn-success" to={"/RegisterPost"}>Publica tu arte</Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Login></Login>
+      )}
+    </>
+  );
+};
 export default Home;
--- frontend/src/pages/RegisterPost/RegisterPost.jsx
+++ frontend/src/pages/RegisterPost/RegisterPost.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import {  setUser } from "./../../reducers/slices/User";
+import { setUser } from "./../../reducers/slices/User";
 import axios from "axios";
+import Header from "../../components/Header";
+import "./RegisterPost.css"
 
 const RegisterPost = () => {
   const dispatch = useDispatch();
@@ -12,67 +14,87 @@ const RegisterPost = () => {
   const handelSutmid = (e) => {
     e.preventDefault();
     let dataForm = Object.fromEntries(new FormData(e.target));
-    let form= new FormData();
+    let form = new FormData();
 
-    form.append("image",dataForm.image);
-    form.append("tex_enty",dataForm.tex_enty);
-    form.append("categorie_id",dataForm.categorie_id);
-    form.append("user_id",data.id);
+    form.append("image", dataForm.image);
+    form.append("tex_enty", dataForm.tex_enty);
+    form.append("categorie_id", dataForm.categorie_id);
+    form.append("user_id", data.id);
     console.log(form);
     const config = {
       headers: {
-        'content-type': 'multipart/form-data'
-      }
-    }
-    axios.post(apiUrl + "ticketRegister", form,config)
-    .then((resp) => {
+        "content-type": "multipart/form-data",
+      },
+    };
+    axios
+      .post(apiUrl + "ticketRegister", form, config)
+      .then((resp) => {
         console.log(resp.data);
-      //dispatch(setUser(resp.data));
-      //navigate("/home");
-    })
-    .catch((error)=>{
-     console.log(error);
-    });
-    
+        //dispatch(setUser(resp.data));
+        //navigate("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
-    <div className="container">
-      <div className="row  vh-100 justify-content-center align-items-center">
-        <div className="col-auto bg-light p-5 shadow-lg">
-          <div className="card-header">
-            <h1 className="text-secondary">Registrar Post</h1>
+    <>
+      <Header></Header>
+      <div className="container">
+        <div className="row  vh-100 justify-content-center align-items-center">
+          <div className="col-auto bg-light p-5 shadow-lg">
+            <div className="card-header">
+              <h1 className="text-secondary">Publica tu arte</h1>
+            </div>
+            <form
+              className="border-bottom border-secondary p-4 form "
+              onSubmit={(e) => handelSutmid(e)}
+            >
+              <div className="mb-3">
+                Imágen
+                <input
+                  type="file"
+                  id="avatar"
+                  name="image"
+                  accept="image/png, image/jpeg"
+                  className="form-control form-control-lg bg-light btn-outline-success text-dark"
+                />
+              </div>
+              <div className="mb-3">
+                Descripción
+                <input
+                  className="form-control mb-3 bg-light btn-outline-success text-dark"
+                  type="text"
+                  name="tex_enty"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                Categoria
+                <select
+                  name="categorie_id"
+                  className="form-select bg-light btn-outline-success text-dark"
+                  required
+                >
+                  <option value="1">Value 1</option>
+                  <option value="1">Value 2</option>
+                  <option value="1">Value 3</option>
+                </select>
+              </div>
+
+              <div className="buttons">
+                <button className="form-login__load btn btn-success">
+                  Publicar
+                </button>
+                <button className="form-login__load btn btn-success">
+                  Cancelar
+                </button>
+              </div>
+            </form>
           </div>
-          <form
-            className="border-bottom border-secondary p-4 form "
-            onSubmit={(e) => handelSutmid(e)}
-          >
-            <div className="mb-3">
-            Img:<input type="file"
-       id="avatar" name="image"
-       accept="image/png, image/jpeg" className="mb-3 bg-light btn-outline-success text-dark"/>
-            </div>
-            <div className="mb-3">
-              Text:<input
-                className="form-control mb-3 bg-light btn-outline-success text-dark"
-                type="text"
-                name="tex_enty"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              Categori:<select name="categorie_id" required>
-                <option value="1">Value 1</option>
-                <option value="1">Value 2</option>
-                <option value="1">Value 3</option>
-              </select>
-            </div>
-            <button className="form-login__load btn btn-success">
-              Registra
-            </button>
-          </form>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
