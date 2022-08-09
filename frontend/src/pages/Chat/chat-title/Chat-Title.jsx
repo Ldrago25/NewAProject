3e5953ad6e8cd60935a634bff711b632e1f201a6
Endersson Enrique Garcia B <endersson.garcia.eegb@gmail.com>
2022-08-08 23:57:44 -0400

    vista chat

---
+++ frontend/src/pages/Chat/chat-title/Chat-Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Chat-Title.css';
+
+function ChatTitle() {
+    return (
+        <div id="chat-title">
+            <span>Daryl Duckmanton</span>
+            <img src="../../../images/icons/trash-logo.svg" alt="Delete Conversation" />
+        </div>
+    );
+}
+
+export default ChatTitle;
--- frontend/src/pages/Perfil/Perfil.jsx
+++ frontend/src/pages/Perfil/Perfil.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect,useState } from 'react'
 import { getUser } from '../../helpers/getUser';
 import WorkGridItem from './WorkGridItem';
-import perfil from "../../img/perfil.jpg";
 import Header from '../../components/Header';
 const Perfil = () => {
     const [images, setImages] = useState([]);
@@ -13,9 +12,9 @@ const Perfil = () => {
             <Header/>
         <div className=' bg-light container'>
             <div className="d-flex justify-content-center align-items-center">
-            <div style={{ width: 180,height:180, margin:10, display: "flex", justifyContent: "center"}}>
-                <img src={perfil} alt="" style={{borderRadius: "50%"}} className='img-responsive'/>
-                <p className='mt-5 mx-2'>UserName</p>
+            <div style={{ width: 150,height:100, margin:10, display: "flex", justifyContent: "center"}}>
+                <img src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg" alt="" style={{borderRadius: "50%"}} className='img-responsive'/>
+                <p className='mt-2 mx-2'>UserName</p>
             </div>
             
             
